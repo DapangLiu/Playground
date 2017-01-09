2a24155d7ff6af09cbde4b1ed5847ca77a6aadf7
Ruibo Liu <rliu68@asu.edu>
2017-01-09 16:40:13 -0700

    sum

---
+++ sum/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+long sum (int n) {
+
+    if (1 == n) {
+        return 1;
+    } else {
+        return n + sum(n-1);
+    }
+}
+
+int main() {
+    printf("%ld\n", sum(100));
+    return 0;
+}
